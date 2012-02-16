d116cd44eb1239e891451f85c803832935ad7195
Dan Woodward <dan@danwoodward.com>
2012-02-16 15:23:05 -0500

    trying another approach for scroll option after plugin update

--- percolate-import.php
+++ percolate-import.php
@@ -69,7 +69,7 @@ class PercolateImport
 		if (get_option(self::LASTIMPORT_OPTION) == FALSE ) update_option(self::LASTIMPORT_OPTION, '0');	
 		if (get_option(self::STARTID_OPTION) == FALSE ) update_option(self::STARTID_OPTION, '0');						
 		if (get_option(self::POSTSTATUS_OPTION) == FALSE ) update_option(self::POSTSTATUS_OPTION, 'publish');
-		if (get_option(self::IMPORT_MOSTRECENT_OPTION) === FALSE ) update_option(self::IMPORT_MOSTRECENT_OPTION,1);						
+		if (get_option(self::IMPORT_MOSTRECENT_OPTION) === 0 ) update_option(self::IMPORT_MOSTRECENT_OPTION,1);						
 		if (get_option(self::ALLSOURCES_OPTION) == FALSE ) update_option(self::ALLSOURCES_OPTION, '0');						
 	}
 
