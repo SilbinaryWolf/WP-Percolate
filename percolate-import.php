aeecf9db90331da0d0024dac23bdf9fd63baf1b4
Dan Woodward <dan@danwoodward.com>
2012-02-17 18:55:28 -0500

    version bump

--- percolate-import.php
+++ percolate-import.php
@@ -7,7 +7,7 @@ Plugin Name: WP Percolate
 Plugin URI: http://percolate.org
 Description: This plugin turns Percolate posts into Wordpress entries.
 Author: Percolate Industries, Inc.
-Version: 2.3.11
+Version: 2.3.12
 Author URI: http://percolate.org
 */
 
