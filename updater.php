8a046a6664bafd5ee140de346ff69d97ace027fe
Dan Woodward <dan@danwoodward.com>
2013-09-19 11:17:58 -0400

    adding updated wp version to updater file

--- updater.php
+++ updater.php
@@ -29,7 +29,7 @@ class GitHubUpdater {
 				'zip_url' => 'https://github.com/percolate/WP-Percolate/zipball/master',
 				'sslverify' => false,
 				'requires' => "3.2.0",
-				'tested' => "3.5.1", //$wp_version
+				'tested' => "3.6.1", //$wp_version
 				);	
 	
 			// Get the config values from the actual plugin we will be updating.
